af48fbbb86a1d0020f52fbd782e947d10ced8429
Fastestkyo <fastestkyo@gmail.com>
2025-01-24 19:14:11 +0530

    upd

---
+++ game_data.py
@@ -0,0 +1,353 @@
+TRAINER_DATA = {
+	'o1': {
+		'monsters': {0: ('Jacana', 14), 1: ('Cleaf', 15), 1: ('Draem', 13)},
+		'dialog': {
+			'default': ['Hey, how are you?', 'Oh, so you want to fight?', 'FIGHT!'], 
+			'defeated': ['You are very strong!', 'Let\'s fight again sometime?']},
+		'directions': ['down'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'o2': {
+		'monsters': {0: ('Finsta', 14), 1: ('Pouch', 15)},
+		'dialog': {
+			'default': ['i don\'t like sand', 'it\'s coarse and idk', 'oh god, fight'], 
+			'defeated': ['may the skbidi be with you']},
+		'directions': ['left', 'down'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'o3': {
+		'monsters': {0: ('Atrox', 14), 1: ('Pouch', 15), 2: ('Gulfin', 13)},
+		'dialog': {
+			'default': ['i love swimming in wet concrete!', 'FIGHT!'], 
+			'defeated': ['gl with the boss', 'it\'s so wet in here']},
+		'directions': ['left', 'right', 'up', 'down'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'o4': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Atrox',24)},
+		'dialog': {
+			'default': ['still water', 'those who know :skull:'], 
+			'defeated': ['glgl', 'it\'s so cold in here']},
+		'directions': ['right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'o5': {
+		'monsters': {0: ('Plumette', 20), 1: ('Ivieron', 22), 2: ('Atrox',24), 3: ('Pouch', 19)},
+		'dialog': {
+			'default': ['I love skating!', 'FIGHT!'], 
+			'defeated': ['i meant ice skating', 'is the water here drinkable?']},
+		'directions': ['up', 'right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'o6': {
+		'monsters': {0: ('Finsta', 15), 1: ('Cleaf', 15), 2: ('Draem', 15)},
+		'dialog': {
+			'default': ['I love to eat sand', 'FIGHT!'], 
+			'defeated': ['bruh wat', 'sand']},
+		'directions': ['down'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'o7': {
+		'monsters': {0: ('Jacana', 25), 1: ('Pouch', 20), 2: ('Atrox',24)},
+		'dialog': {
+			'default': ['There are no bugs in the snow!'], 
+			'defeated': ['Maybe I should check a volcano...', 'It\'s so cold in here']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'p1': {
+		'monsters': {0: ('Plumette', 28), 1: ('Cleaf', 27), 2: ('Jacana',29)},
+		'dialog': {
+			'default': ['pojpojpnpkcnaosopj[ihn[inoibasnfklna.]]'], 
+			'defeated': ['sigma']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'p2': {
+		'monsters': {0: ('Cleaf', 30), 1: ('Sparchu', 29)},
+		'dialog': {
+			'default': ['e'], 
+			'defeated': ['easdaojsdaposk']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'p3': {
+		'monsters': {0: ('Ivieron', 27)},
+		'dialog': {
+			'default': ['idk what to type here'], 
+			'defeated': ['nice ig']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'p4': {
+		'monsters': {0: ('Jacana', 28), 1: ('Draem', 26)},
+		'dialog': {
+			'default': ['no', 'pls fights'], 
+			'defeated': ['yk any good therapists around here?']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'px': {
+		'monsters': {0: ('Pluma', 33), 1: ('Cleaf', 29), 2: ('Atrox',29), 3: ('Ivieron', 30)},
+		'dialog': {
+			'default': ['I love trees', 'and fights'], 
+			'defeated': ['i hate trees']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'w1': {
+		'monsters': {0: ('Gulfin', 25), 1: ('Gulfin', 27)},
+		'dialog': {
+			'default': ['skibidi skibidi skibidi skibidi', 'our fight will be like cameraman vs toilet'], 
+			'defeated': ['skibidi dop dop dop']},
+		'directions': ['left'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'w2': {
+		'monsters': {0: ('Jacana', 28), 1: ('Gulfin', 29), 2: ('Draem',28)},
+		'dialog': {
+			'default': ['*sigh*'], 
+			'defeated': ['...']},
+		'directions': ['right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'w3': {
+		'monsters': {0: ('Atrox', 27), 1: ('Cleaf', 27), 2: ('Draem',28)},
+		'dialog': {
+			'default': ['-_-'], 
+			'defeated': ['...']},
+		'directions': ['left'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'w4': {
+		'monsters': {0: ('Friolera', 28), 1: ('Gulfin', 28), 2: ('Ivieron', 28)},
+		'dialog': {
+			'default': ['i wanna eat lunch rn', 'finish this or i eat u'], 
+			'defeated': ['ur cooked']},
+		'directions': ['right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'w5': {
+		'monsters': {0: ('Gulfin', 27), 1: ('Finsta', 26), 2: ('Draem',24)},
+		'dialog': {
+			'default': ['It\'s so cold in here', 'maybe a fight will warm me up'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['left'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'wx': {
+		'monsters': {0: ('Jacana', 27), 1: ('Gulfin', 29), 2: ('Draem',28), 3: ('Finiette', 33)},
+		'dialog': {
+			'default': ['bruh'], 
+			'defeated': ['e']},
+		'directions': ['down'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'f1': {
+		'monsters': {0: ('Cindrill', 25), 1: ('Jacana', 26)},
+		'dialog': {
+			'default': ['i wanna go take a shit rn', 'end this fast'], 
+			'defeated': ['alr thx']},
+		'directions': ['right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'f2': {
+		'monsters': {0: ('Sparchu', 25), 1: ('Draem', 28), 2: ('Finsta',28)},
+		'dialog': {
+			'default': ['I\'m cooked', 'pls lemme win'], 
+			'defeated': ['i hate u']},
+		'directions': ['right', 'left'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'f3': {
+		'monsters': {0: ('Larvea', 27), 1: ('Friolera', 29), 2: ('Atrox', 28)},
+		'dialog': {
+			'default': ['bro the skibidi toilet episode was fire!!!'], 
+			'defeated': ['lets watch skibidi toilet!', 'the lore is insane!!']},
+		'directions': ['right', 'left'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'f4': {
+		'monsters': {0: ('Cindrill', 29)},
+		'dialog': {
+			'default': ['i like microwaves'], 
+			'defeated': ['i like ceiling fans']},
+		'directions': ['up', 'right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'f5': {
+		'monsters': {0: ('Cindrill', 27), 1: ('Jacana', 28), 2: ('Friolera',26)},
+		'dialog': {
+			'default': ['OUR FIGHT WILL BE FIRE!'], 
+			'defeated': ['bruh wtf']},
+		'directions': ['left'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'f6': {
+		'monsters': {0: ('Cindrill', 25), 1: ('Finsta', 26), 2: ('Draem',24), 3: ('Atrox', 30)},
+		'dialog': {
+			'default': ['This place feels kinda warm...', 'You know what else is warm??', 'FIGHT!!'], 
+			'defeated': ['nooo i almost won!!']},
+		'directions': ['right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'fx': {
+		'monsters': {0: ('Pouch', 25), 1: ('Draem', 27), 2: ('Charmadillo',33), 3: ('Plumette', 30)},
+		'dialog': {
+			'default': ['Time to bring the heat', 'fight!'], 
+			'defeated': ['Congratultion!']},
+		'directions': ['down'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'Nurse': {
+		'direction': 'down',
+		'radius': 0,
+		'look_around': False,
+		'dialog': {
+			'default': ['Welcome to the hospital', 'Your monsters have been healed'], 
+			'defeated': None},
+		'directions': ['down'],
+		'defeated': False,
+		'biome': None
+		}
+}
+
+MONSTER_DATA = {
+	'Plumette': {
+		'stats': {'element': 'plant', 'max_health': 15, 'max_energy': 17, 'attack': 4, 'defense': 8, 'recovery': 5, 'speed': 1},
+		'abilities': {0: 'scratch', 5: 'green day',9: 'root snare'},
+		'evolve': ('Ivieron', 15)},
+	'Ivieron': {
+		'stats': {'element': 'plant', 'max_health': 18, 'max_energy': 20, 'attack': 5, 'defense': 10, 'recovery': 6, 'speed': 1.2},
+		'abilities': {0: 'scratch', 5: 'green day', 9: 'root snare', 15:'terrain pulse', 25:'bloom'},
+		'evolve': ('Pluma', 32)},
+	'Pluma': {
+		'stats': {'element': 'plant', 'max_health': 23, 'max_energy': 25, 'attack': 6, 'defense': 12, 'recovery': 7, 'speed': 1.8},
+		'abilities': {0: 'scratch', 5: 'green day', 9: 'root snare', 15:'terrain pulse', 25:'bloom', 40:'skibidi', 60:'still water'},
+		'evolve': None},
+	'Sparchu': {
+		'stats': {'element': 'fire', 'max_health': 15, 'max_energy': 17, 'attack': 3, 'defense': 8, 'recovery': 5, 'speed': 1},
+		'abilities': {0: 'scratch', 5: 'spark', 11:'burn'},
+		'evolve': ('Cindrill', 15)},
+	'Cindrill': {
+		'stats': {'element': 'fire', 'max_health': 18, 'max_energy': 20, 'attack': 4, 'defense': 10, 'recovery': 6, 'speed': 1.2},
+		'abilities': {0: 'scratch', 5: 'spark', 11:'burn', 15:'fire', 25:'explosion'},
+		'evolve': ('Charmadillo', 33)},
+	'Charmadillo': {
+		'stats': {'element': 'fire', 'max_health': 27, 'max_energy': 23, 'attack': 6, 'defense': 17, 'recovery': 7, 'speed': 1.5},
+		'abilities': {0: 'scratch', 5: 'spark', 11:'burn', 15:'fire', 25:'explosion', 40:'annihilate', 60:'tear'},
+		'evolve': None},
+	'Finsta': {
+		'stats': {'element': 'water', 'max_health': 13, 'max_energy': 17, 'attack': 2, 'defense': 8, 'recovery': 5, 'speed': 1.8},
+		'abilities': {0: 'scratch', 5: 'ice', 11:'water wave'},
+		'evolve': ('Gulfin', 14)},
+	'Gulfin': {
+		'stats': {'element': 'water', 'max_health': 18, 'max_energy': 20, 'attack': 3, 'defense': 10, 'recovery': 6, 'speed': 2},
+		'abilities': {0: 'scratch', 5: 'ice', 11:'water wave', 18:'splash', 30:'smash'},
+		'evolve': ('Finiette', 32)},
+	'Finiette': {
+		'stats': {'element': 'water', 'max_health': 27, 'max_energy': 23, 'attack': 4, 'defense': 17, 'recovery': 7, 'speed': 2.5},
+		'abilities': {0: 'scratch', 5: 'ice', 11:'water wave', 18:'splash', 30:'smash', 50:'still water', 70:'battlecry'},
+		'evolve': None},
+	'Atrox': {
+		'stats': {'element': 'normal', 'max_health': 18, 'max_energy': 20, 'attack': 3, 'defense': 10, 'recovery': 6, 'speed': 1.9},
+		'abilities': {0: 'scratch', 5: 'spark', 13:'smash', 24:'splash', 35:'skibidi', 50:'fire', 75:'annihilate'},
+		'evolve': None},
+	'Pouch': {
+		'stats': {'element': 'normal', 'max_health': 23, 'max_energy': 25, 'attack': 4, 'defense': 12, 'recovery': 7, 'speed': 1.5},
+		'abilities': {0: 'scratch', 5: 'green day', 13:'smash', 24:'splash', 35:'skibidi', 50:'tear', 75:'bloon'},
+		'evolve': None},
+	'Draem': {
+		'stats': {'element': 'normal', 'max_health': 23, 'max_energy': 25, 'attack': 4, 'defense': 12, 'recovery': 7, 'speed': 1.4},
+		'abilities': {0: 'scratch', 5: 'tear', 15:'smash', 24:'drink water', 35:'battlecry', 50:'skibidi', 75:'still water'},
+		'evolve': None},
+	'Larvea': {
+		'stats': {'element': 'plant', 'max_health': 15, 'max_energy': 17, 'attack': 1, 'defense': 8, 'recovery': 5, 'speed': 1},
+		'abilities': {0: 'scratch', 5: 'root snare'},
+		'evolve': ('Cleaf', 4)},
+	'Cleaf': {
+		'stats': {'element': 'plant', 'max_health': 18, 'max_energy': 20, 'attack': 3, 'defense': 10, 'recovery': 6, 'speed': 1.6},
+		'abilities': {0: 'scratch', 5: 'root snare', 14:'terrain pulse', 24:'tear', 35:'splash', 50:'bloom'},
+		'evolve': None},
+	'Jacana': {
+		'stats': {'element': 'normal', 'max_health': 12, 'max_energy': 19, 'attack': 3, 'defense': 10, 'recovery': 6, 'speed': 2.6},
+		'abilities': {0: 'scratch', 5: 'tear', 15:'smash', 20:'drink water', 25:'battlecry', 40:'skibidi'},
+		'evolve': None},
+	'Friolera': {
+		'stats': {'element': 'water', 'max_health': 27, 'max_energy': 23, 'attack': 4, 'defense': 17, 'recovery': 7, 'speed': 2},
+		'abilities': {0: 'scratch', 5: 'splash', 15:'tear', 20:'drink water', 25:'explosion', 40:'annihilate'},
+		'evolve': None},
+}
+
+ATTACK_DATA = {
+	'heal':       		{'target': 'player',   'amount': -1.2, 'cost': 15, 'element': 'plant',  'animation': 'green'},
+ 	'green day':  		{'target': 'opponent', 'amount': 1.1,  'cost': 20, 'element': 'plant', 'animation': 'green'},	
+  	'root snare': 		{'target': 'opponent', 'amount': 1.3,  'cost': 40, 'element': 'plant', 'animation': 'green'},	
+   'terrain pulse':	{'target': 'opponent', 'amount': 1.7,  'cost': 70, 'element': 'plant', 'animation': 'green'},
+   'bloom': 			{'target': 'opponent', 'amount': 2.1,  'cost': 100, 'element': 'plant', 'animation': 'green'},		
+	'battlecry':  		{'target': 'opponent',   'amount': 1.4,  'cost': 20, 'element': 'normal', 'animation': 'green'},
+	'scratch':    		{'target': 'opponent', 'amount': 1.2,  'cost': 20, 'element': 'normal', 'animation': 'scratch'},
+ 	'smash':    		{'target': 'opponent', 'amount': 1.4,  'cost': 50, 'element': 'normal', 'animation': 'scratch'},
+	'tear':    			{'target': 'opponent', 'amount': 1.2,  'cost': 70, 'element': 'normal', 'animation': 'scratch'},
+	'skibidi':    		{'target': 'opponent', 'amount': 2.3,  'cost': 100, 'element': 'normal', 'animation': 'scratch'},
+	'splash':     		{'target': 'opponent', 'amount': 0.01,    'cost': 0, 'element': 'water',  'animation': 'splash'},
+	'ice':        		{'target': 'opponent', 'amount': 1.8,    'cost': 75, 'element': 'water',  'animation': 'ice'},
+	'still water':    {'target': 'opponent', 'amount': 2.1,    'cost': 110, 'element': 'water',  'animation': 'ice'},#thosewhoknow :skull:
+	'water wave':     {'target': 'opponent', 'amount': 1.5,    'cost': 55, 'element': 'water',  'animation': 'ice'},
+ 	'drink water':    {'target': 'player', 'amount': -2,    'cost': 100, 'element': 'water',  'animation': 'ice'},
+	'spark':      		{'target': 'opponent', 'amount': 1.1,  'cost': 20, 'element': 'fire',   'animation': 'fire'},
+	'burn':       		{'target': 'opponent', 'amount':1.4,    'cost': 65, 'element': 'fire',   'animation': 'fire'},
+	'fire':       		{'target': 'opponent', 'amount': 1.6,    'cost': 85, 'element': 'fire',   'animation': 'fire'},
+	'explosion':  		{'target': 'opponent', 'amount': 2,    'cost': 90, 'element': 'fire',   'animation': 'explosion'},
+	'annihilate': 		{'target': 'opponent', 'amount': 3,    'cost': 150, 'element': 'fire',   'animation': 'explosion'},
+}
